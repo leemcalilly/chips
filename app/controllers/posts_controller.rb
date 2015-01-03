7537edcc181487660ed8dcc897def7eeb416338b
Lee McAlilly <leemcalilly@gmail.com>
2015-01-02 20:34:12 -0600

    adding all the places we've been so far

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,21 @@
 class PostsController < ApplicationController
   layout "post"
 
-  def las_maracas_family_dollar
+  def cinco_de_mayo_cummins_station
+  end
+
+  def el_torero
+  end
+
+  def las_maracas_family_dollar_1184
+  end
+
+  def las_palmas_franklin_rd
+  end
+
+  def little_mexico
+  end
+
+  def rose_pepper
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   get '/banos', to: 'pages#banos'
 
   # Posts
-  get '/las-maracas-family-dollar-1184', to: 'posts#las_maracas_family_dollar'
+  get '/cinco-de-mayo-cummins-station', to: 'posts#cinco_de_mayo_cummins_station'
+  get '/el-torero', to: 'posts#el_torero'  
+  get '/las-maracas-family-dollar-1184', to: 'posts#las_maracas_family_dollar_1184'
+  get '/las-palmas-franklin-rd', to: 'posts#las_palmas_franklin_rd'
+  get '/little-mexico', to: 'posts#little_mexico'
+  get '/rose-pepper', to: 'posts#rose_pepper'
+
 end
